b69055dd07b2c8b23fb4e9aa72bee76dfb0aa05c
github username <github email address>
2018-11-28 17:56:08 -0500

    small changes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,6 +18,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password_digest)
+    params.require(:user).permit(:username)
+    # , :password_digest)
   end
 end
