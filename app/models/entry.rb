8aaf48beeb28d2a427964de474af7578f174ac0a
github username <github email address>
2018-12-01 23:22:05 -0500

    no more time_left in backend / json

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -14,6 +14,22 @@ class Entry < ApplicationRecord
   end
 
 
+  def status
+    start = self.entry_date_and_time
+    duration_sec = self.insulin.insulin_duration_in_minutes * 60
+    end_time = (start + duration_sec).to_time
+    now = Time.now.getlocal
+    remaining = end_time - now
+    if remaining <= 0
+      return "complete"
+    else
+      return "active"
+    end
+  end
+
+end
+
+
   # def formatted_date
   #   self.entry_date_and_time.strftime("%m/%d/%Y")
   # end
@@ -22,11 +38,11 @@ class Entry < ApplicationRecord
   #   self.entry_date_and_time.strftime("%I:%M %p")
   # end
   #
-  def end_time
-    start = self.entry_date_and_time
-    duration_sec = self.insulin.insulin_duration_in_minutes * 60
-    (start + duration_sec).to_time
-  end
+  # def end_time
+  #   start = self.entry_date_and_time
+  #   duration_sec = self.insulin.insulin_duration_in_minutes * 60
+  #   (start + duration_sec).to_time
+  # end
   # #
   # def formatted_end_time
   #   self.end_time.strftime("%I:%M %p")
@@ -35,23 +51,13 @@ class Entry < ApplicationRecord
   # def now
   #   Time.now.getlocal.strftime("%I:%M %p")
   # end
-
-  def time_left
-    now = Time.now.getlocal
-    remaining = self.end_time - now
-    if remaining > 0
-      Time.at(remaining).utc.strftime('%H:%M')
-    else
-      "-"
-    end
-  end
-
-  def status
-    if self.time_left == "-"
-      return "complete"
-    else
-      return "active"
-    end
-  end
-
-end
+  #
+  # def time_left
+  #   now = Time.now.getlocal
+  #   remaining = self.end_time - now
+  #   if remaining > 0
+  #     Time.at(remaining).utc.strftime('%H:%M')
+  #   else
+  #     "-"
+  #   end
+  # end
