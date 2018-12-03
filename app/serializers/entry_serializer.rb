32b2bf83169a871f6b7199519c884567eaa29f95
github username <github email address>
2018-12-03 12:39:01 -0500

    changed entry serializer

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,5 +1,7 @@
 class EntrySerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :entry_date_and_time, :insulin_id, :status, :note
+  attributes :id, :user_id, :entry_date_and_time,
+  :insulin_id, :status, :note,
+  :current_blood_sugar, :num_units_insulin
 
 
 end
