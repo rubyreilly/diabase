b234d7131bdf08efc1d63cb737a78d9bc10038a4
github username <github email address>
2018-12-05 23:50:27 -0500

    styling and time

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :entries
   # has_secure_password
   validates :username, uniqueness: { case_sensitive: false }
+  
 
   def insulins_info
     insulins = self.insulins.map do |i|
